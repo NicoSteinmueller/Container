914c21f3e2685f70de770f91ba8a0aec87af7ae2
Nico Steinmüller <nico.steinmueller@gmx.net>
2026-07-20 00:06:29 +0200

    Refactor Terraform configuration for Ubuntu 24.04 VM setup and update .gitignore

--- vm/ubuntu-desktop/main.tf
+++ vm/ubuntu-desktop/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.9.8" # Nutzt die aktuelle Version des Providers
+      version = "0.9.8"
     }
   }
 }
@@ -11,10 +11,13 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-# 1. Ubuntu Cloud Image als Basis herunterladen
+#
+# Ubuntu 24.04 Cloud Image
+#
 resource "libvirt_volume" "ubuntu_base" {
-  name = "ubuntu-24.04.qcow2"
+  name = "ubuntu-24.04-base.qcow2"
   pool = "default"
+
   target = {
     format = {
       type = "qcow2"
@@ -26,44 +29,56 @@ resource "libvirt_volume" "ubuntu_base" {
       url = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
     }
   }
-
 }
 
-# 2. Eigene Festplatte für die VM aus der Basis erstellen (Größe anpassen!)
+#
+# Writable overlay
+#
 resource "libvirt_volume" "ubuntu_disk" {
-  name     = "ubuntu-desktop-root.qcow2"
-  pool     = "default"
-  capacity = 25 * 1024 * 1024 * 1024 # 25 GB Speicherplatz
+  name = "ubuntu-desktop.qcow2"
+  pool = "default"
+
   target = {
     format = {
       type = "qcow2"
     }
   }
+
+  capacity = 25 * 1024 * 1024 * 1024 # 25 GB
+
   backing_store = {
     path = libvirt_volume.ubuntu_base.path
+
     format = {
       type = "qcow2"
     }
   }
 }
 
-# 3. Cloud-Init Konfiguration einbinden
+#
+# Cloud-init
+#
 resource "libvirt_cloudinit_disk" "ubuntu_seed" {
-  name      = "ubuntu-cloudinit"
+  name = "ubuntu-cloudinit"
+
   user_data = file("${path.module}/cloud_init.cfg")
+
   meta_data = yamlencode({
     instance_id    = "ubuntu-desktop"
     local_hostname = "ubuntu-desktop"
   })
-  network_config = <<-EOF
-    version: 2
-    ethernets:
-      eth0:
-        dhcp4: true
-    EOF
+
+  network_config = <<EOF
+version: 2
+ethernets:
+  eth0:
+    dhcp4: true
+EOF
 }
 
-# replace cloud-init ISO into the pool
+#
+# Upload ISO
+#
 resource "libvirt_volume" "ubuntu_seed_volume" {
   name = "ubuntu-cloudinit.iso"
   pool = "default"
@@ -75,8 +90,10 @@ resource "libvirt_volume" "ubuntu_seed_volume" {
   }
 }
 
-# 4. Die eigentliche VM definieren
-resource "libvirt_domain" "ubuntu_desktop_vm" {
+#
+# VM
+#
+resource "libvirt_domain" "ubuntu" {
   name        = "ubuntu-24.04-desktop"
   memory      = 4096
   memory_unit = "MiB"
@@ -87,11 +104,6 @@ resource "libvirt_domain" "ubuntu_desktop_vm" {
     type         = "hvm"
     type_arch    = "x86_64"
     type_machine = "q35"
-    boot_devices = [
-      {
-        dev = "hd"
-      }
-    ]
   }
 
   devices = {
@@ -99,35 +111,45 @@ resource "libvirt_domain" "ubuntu_desktop_vm" {
       {
         source = {
           volume = {
-            pool = libvirt_volume.ubuntu_disk.pool
+            pool   = libvirt_volume.ubuntu_disk.pool
             volume = libvirt_volume.ubuntu_disk.name
           }
         }
+
         target = {
           dev = "vda"
           bus = "virtio"
         }
+
+        driver = {
+          type = "qcow2"
+        }
       },
       {
         device = "cdrom"
+
         source = {
-            volume = {
-                pool = libvirt_volume.ubuntu_seed_volume.pool
-                volume = libvirt_volume.ubuntu_seed_volume.name
-            }
+          volume = {
+            pool   = libvirt_volume.ubuntu_seed_volume.pool
+            volume = libvirt_volume.ubuntu_seed_volume.name
+          }
         }
+
         target = {
           dev = "sda"
-            bus = "sata"
+          bus = "sata"
         }
       }
     ]
+
     interfaces = [
       {
         type = "network"
+
         model = {
           type = "virtio"
         }
+
         source = {
           network = {
             network = "default"
@@ -136,12 +158,14 @@ resource "libvirt_domain" "ubuntu_desktop_vm" {
       }
     ]
 
-    graphics = [{
-      spice = {
-        auto_port = true
-        listen    = "127.0.0.1"
+    graphics = [
+      {
+        spice = {
+          auto_port = true
+          listen    = "127.0.0.1"
+        }
       }
-    }]
+    ]
   }
 
   running = true
